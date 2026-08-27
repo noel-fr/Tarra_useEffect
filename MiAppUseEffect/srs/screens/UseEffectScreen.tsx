1196b45a99eb672c1d2256132382e4d655a22f2f
noel-fr <fnoel6050@gmail.com>
2026-08-26 22:17:23 -0600

    --Estructura inicial de carpetas y creacion de la estructura inicial del componente

---
+++ MiAppUseEffect/srs/screens/UseEffectScreen.tsx
@@ -0,0 +1,13 @@
+
+import {View,Text,TouchableOpacity,StyleSheet} from "react-native";
+import { useState } from "react";
+
+const UseEffectScreen = () => {
+    return(
+        <View>
+            <Text>Práctica UseEffect</Text>
+        </View>
+    );
+};
+
+export default UseEffectScreen;
