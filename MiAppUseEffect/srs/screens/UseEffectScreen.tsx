ab81019b1653a6a1f6f0c04a61bc62016e2a87bc
noel-fr <fnoel6050@gmail.com>
2026-08-26 22:50:44 -0600

    --Creacion del contador inicial con useState, el cual comienza en cero y se puede aumentar al precionar el boton.

--- MiAppUseEffect/srs/screens/UseEffectScreen.tsx
+++ MiAppUseEffect/srs/screens/UseEffectScreen.tsx
@@ -3,9 +3,15 @@ import {View,Text,TouchableOpacity,StyleSheet} from "react-native";
 import { useState } from "react";
 
 const UseEffectScreen = () => {
+    const [contador, setContador] = useState(0);
     return(
         <View>
             <Text>Práctica UseEffect</Text>
+            <Text>Contador: {contador}</Text>
+            <TouchableOpacity onPress={() => setContador(contador + 1)}>
+                <Text>Aumentar</Text>
+            </TouchableOpacity>
+            
         </View>
     );
 };
