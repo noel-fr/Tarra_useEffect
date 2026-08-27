3bc271db7bb8af00f43fce5061f9460985c8cccd
noel-fr <fnoel6050@gmail.com>
2026-08-26 23:07:39 -0600

    -- Agregue el primer useEffect para observar cuando se renderiza el componente

--- MiAppUseEffect/srs/screens/UseEffectScreen.tsx
+++ MiAppUseEffect/srs/screens/UseEffectScreen.tsx
@@ -1,9 +1,26 @@
 
 import {View,Text,TouchableOpacity,StyleSheet} from "react-native";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const UseEffectScreen = () => {
     const [contador, setContador] = useState(0);
+
+    /*
+        *useEffect sin arreglo de dependencias
+        *
+        * Este useEffect se ejecuta despues de cada renderizado del componente
+        * porque no tiene un arreglo de dependecias
+        * 
+        * Se puede puede utilizar cuando necesitamos ejecutar una acción cada vez
+        * que el componente se actualiza.
+        * 
+        * (Espero estar en lo correcto ingeniera) 
+    */
+
+    useEffect (() =>{
+        console.log("El componente se renderizó");
+    });
+
     return(
         <View>
             <Text>Práctica UseEffect</Text>
